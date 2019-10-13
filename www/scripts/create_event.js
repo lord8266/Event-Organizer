2c5046abbbe70487409fcb977c22ea9c9f9760ab
lordvishwa123@gmail.com <lord8266>
2019-10-13 21:47:46 +0530

    moving in completion

--- www/scripts/create_event.js
+++ www/scripts/create_event.js
@@ -1,14 +1,24 @@
 
+var date_picker_start;
+var date_picker_end;
+var time_picker_start;
+var time_picker_end;
+var login_dropdown;
+var collapisble_front;
+var collapsible_tal;
+var tabs;
+
 $('document').ready(
     function() {
-        $('.tabs').tabs();
-        $('.dropdown-trigger').dropdown();
-        $('#collapsible_front').collapsible();
-        $('#collapsible_tal').collapsible();
-        $('#date_start').datepicker();
-        $('#date_end').datepicker();
-        console.log( $('#time_start').timepicker());
-        $('#time_end').timepicker();
+        
+        tabs = M.Tabs.init( $('.tabs')[0]) ;
+        login_dropdown = M.Dropdown.init( $('.dropdown-trigger')[0]) ;
+        collapisble_front = M.Collapsible.init( $('#collapsible_front')[0]) ;
+        collapsible_tal = M.Collapsible.init( $('#collapsible_tal')[0]) ;
+        date_picker_start =M.Datepicker.init( $('#date_start')[0],{format: "mm/dd/yyyy",minDate: new Date(),onClose:()=>{on_close('s')}  }) ;
+        date_picker_end = M.Datepicker.init( $('#date_end')[0],{format: "mm/dd/yyyy",minDate: new Date(),onClose:()=>{on_close('e')} ,onOpen:()=>{on_open('e')} }) ;
+        time_picker_start= M.Timepicker.init( $('#time_start')[0] );
+        time_picker_start = M.Timepicker.init( $('#time_end')[0] );
         $('#modal_add_tags').modal();
         $('#tags').data('count',0);
         $('#tags').data('max',4);
@@ -17,13 +27,44 @@ $('document').ready(
     }
 
 );
-function price_show() {
-    if ($('#check_paid').prop('checked')) {
-        $('#price_div').show()
+
+function on_close(t) {
+    var s0 = date_picker_start.toString()
+    var s1 = date_picker_end.toString()
+    if (t=='s') {
+        if (s1) {
+            if (new Date(s0) >new Date(s1)) {
+                date_picker_end.setDate("")
+                date_picker_end.setInputValue("")
+            }
+        
+        }
+        date_picker_end.options.minDate = date_picker_start.date
     }
-    else {
-        $('#price_div').hide()
-    }
+    else if (t=='e') {
+
+        if (!s0) {
+            date_picker_end.setDate("")
+            date_picker_end.setInputValue("")
+            M.toast( {html:"Select Start Date"} );
+            date_picker_end.open()
+     : Nov 11,
+     : Nov 11,
+    }: Nov 11,
+}
+
+funct: Nov 11,
+  
+    
+}
+
+funct: Nov 11,
+    i: Nov 11,
+     : Nov 11,
+    }: Nov 11,
+    e: Nov 11,
+     : Nov 11,
+    }: Nov 11,
 }
 function rem_tag(e) { 
     $(e.currentTarget).remove()
@@ -56,13 +97,5 @@ function add_tag(e) {
     
 }
 $('#add_tag').click(add_tag)
-// function update() {
-//     var c =`rgba(${$('#R').val()},${$('#G').val()},${$('#B').val()},${$('#A').val()})`;
-//     console.log(c)
-//     $('#profile').css("border-color",c);
-// }
-// $('#R').on('input',()=> {update()});
-// $('#G').on('input',()=> {update()});
-// $('#B').on('input',()=> {update()});
-// $('#A').on('input',()=> {update()});
+
 
